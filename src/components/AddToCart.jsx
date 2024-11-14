bc4c9dcae4dadf2026d04f96add7eae0087a7844
alanb4rt <al4n.b4rt@gmail.com>
2024-11-14 15:14:00 +0100

    refacto ProductPage

---
+++ src/components/AddToCart.jsx
@@ -0,0 +1,36 @@
+import { useContext, useEffect, useState } from "react";
+import Button from "../components/Button";
+import IconCart from "../components/icons/IconCart";
+import QuantitySelector from "../components/QuantitySelector";
+import { CartContext } from "../contexts/CartProvider";
+
+export default function AddToCart({ productId }) {
+  const { cartItems, setCartItems } = useContext(CartContext);
+  const [quantityItem, setQuantityItem] = useState(0);
+
+  const handleQuantity = (quantity) => setQuantityItem(quantity);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (quantityItem === 0) return;
+
+    const existingItem = cartItems.find(({ id }) => id === productId);
+    const updatedCartItems = existingItem
+      ? cartItems.map((item) =>
+          item.id === productId ? { ...item, quantity: quantityItem } : item
+        )
+      : [...cartItems, { id: productId, quantity: quantityItem }];
+    setCartItems(updatedCartItems);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex items-center gap-4">
+      <QuantitySelector handleQuantity={handleQuantity} />
+      <Button type="submit">
+        <IconCart color="var(--very-dark-blue)" />
+        Add to cart
+      </Button>
+    </form>
+  );
+}
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,43 +1,37 @@
-import Button from "../components/Button";
-import IconCart from "../components/icons/IconCart";
+import AddToCart from "../components/AddToCart";
 import ProductImageViewer from "../components/ProductImageViewer";
-import QuantitySelector from "../components/QuantitySelector";
 import Products from "../data/products.json";
 
 export default function ProductPage({ productId }) {
   const product = Products.find(({ id }) => id === productId);
 
-  const discountedPrice = (product.price * (product.discount / 100)).toFixed(2);
+  const { id, images, category, name, description, discount, price } = product;
+
+  const discountedPrice = (price * (discount / 100)).toFixed(2);
 
   return (
     <main>
       <div className="w-full max-w-screen-lg mx-auto p-16">
         <div className="grid md:grid-cols-2 items-center gap-24">
-          <ProductImageViewer productImages={product.images} />
+          <ProductImageViewer productImages={images} />
           <section className="flex flex-col gap-4">
             <p className="font-bold text-[var(--dark-grayish-blue)] tracking-widest uppercase">
-              {product.category}
+              {category}
             </p>
-            <h1 className="text-4xl font-bold mb-4">{product.name}</h1>
+            <h1 className="text-4xl font-bold mb-4">{name}</h1>
             <p className="text-sm text-[var(--dark-grayish-blue)]">
-              {product.description}
+              {description}
             </p>
             <div className="flex items-end gap-4">
               <p className="text-3xl font-bold">${discountedPrice}</p>
               <p className="text-sm font-bold text-white bg-[var(--very-dark-blue)] rounded px-2 py-[2px]">
-                {product.discount}%
+                {discount}%
               </p>
             </div>
             <p className="line-through font-bold text-[var(--dark-grayish-blue)]">
-              ${product.price.toFixed(2)}
+              ${price.toFixed(2)}
             </p>
-            <div className="flex items-center gap-4">
-              <QuantitySelector />
-              <Button>
-                <IconCart color="var(--very-dark-blue)" />
-                Add to cart
-              </Button>
-            </div>
+            <AddToCart productId={id} />
           </section>
         </div>
       </div>
