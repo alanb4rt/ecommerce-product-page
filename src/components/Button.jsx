77163965b48492a77f86639c134ee43ea5784825
alanb4rt <al4n.b4rt@gmail.com>
2024-11-26 11:03:34 +0100

    add hover on Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ type, children }) {
   return (
     <button
       type={type}
-      className="w-full flex items-center justify-center gap-4 bg-[var(--color-primary)] font-bold p-3 rounded-lg"
+      className="w-full flex items-center justify-center gap-4 bg-[var(--color-primary)] hover:bg-[#ffac6a] font-bold p-3 rounded-lg"
     >
       {children}
     </button>
