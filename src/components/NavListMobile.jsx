b929eb4b345ca1a04dee8286b15a3644fa201d06
alanb4rt <al4n.b4rt@gmail.com>
2024-11-27 11:52:30 +0100

    add animation on the mobile menu

--- src/components/NavListMobile.jsx
+++ src/components/NavListMobile.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import IconClose from "./icons/IconClose";
 import { navigations } from "../data/navigations";
 
@@ -5,32 +6,54 @@ export default function NavListMobile({
   isMobileMenuOpen,
   setIsMobileMenuOpen,
 }) {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    if (isMobileMenuOpen) setIsAnimating(true);
+  }, [isMobileMenuOpen]);
+
+  const handleMenuClose = () => {
+    setIsAnimating(false);
+    const animationDuration = 500;
+    setTimeout(() => setIsMobileMenuOpen(false), animationDuration);
+  };
+
   return (
-    <section className={isMobileMenuOpen ? "block" : "hidden"}>
+    <>
       {isMobileMenuOpen && (
-        <div className="z-10 fixed inset-0 bg-black/75">
-          <nav className="w-3/4 h-full p-6 bg-white">
-            <button
-              className="text-[var(--dark-grayish-blue)] hover:text-[var(--color-primary)] mb-10"
-              onClick={() => setIsMobileMenuOpen(false)}
+        <section className="z-50 relative">
+          <div
+            className={`fixed inset-0 bg-black transition-colors duration-500 ${
+              isAnimating ? "bg-opacity-75" : "bg-opacity-0"
+            }`}
+          >
+            <nav
+              className={`w-3/4 h-full p-6 bg-white transition-transform duration-500 ${
+                isAnimating ? "translate-x-0" : "-translate-x-full"
+              }`}
             >
-              <IconClose color="currentcolor" />
-            </button>
-            <ul className="flex flex-col gap-4 text-[var(--dark-grayish-blue)]">
-              {navigations.map(({ name, url }) => (
-                <li key={name} className="nav-list w-fit pb-1">
-                  <a
-                    className="flex items-center h-full font-bold text-[var(--very-dark-blue)]"
-                    href={url}
-                  >
-                    {name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
+              <button
+                className="text-[var(--dark-grayish-blue)] hover:text-[var(--color-primary)] mb-10"
+                onClick={handleMenuClose}
+              >
+                <IconClose color="currentcolor" />
+              </button>
+              <ul className="flex flex-col gap-4 text-[var(--dark-grayish-blue)]">
+                {navigations.map(({ name, url }) => (
+                  <li key={name} className="nav-list w-fit pb-1">
+                    <a
+                      className="flex items-center h-full font-bold text-[var(--very-dark-blue)]"
+                      href={url}
+                    >
+                      {name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </section>
       )}
-    </section>
+    </>
   );
 }
